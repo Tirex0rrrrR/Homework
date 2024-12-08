f6c90614ed26577fe9a17a7472e3b8df72a9b2da
Tirex0rrrrR <lilipimp@mail.ru>
2024-12-08 23:58:03 +0300

    Add files via upload

---
+++ Laba_4/2_ex.py
@@ -0,0 +1,14 @@
+import json
+
+INPUT_FILENAME = "input.json"
+
+def task() -> float:
+
+    with open(INPUT_FILENAME, mode='r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+
+    total_sum = sum(item["score"] * item["weight"] for item in data)
+
+    return round(total_sum, 3)
+
+print(task())
